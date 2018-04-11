3c0353219280a5f7602a6a646eee2f48d82f9a8a
Michael Wood <mwood@data-crush.com>
2018-04-10 21:49:45 -0400

    Chapter 02 Changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+/* globals fetch */
 import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  componentDidMount () {
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=London,ca&appid=2bc782ac273d090fe409ddf32b73cc9c')
+      .then((response) => {
+        return response.json()
+      })
+      .then((json) => {
+        console.log(json)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   render () {
     return (
       <div>
